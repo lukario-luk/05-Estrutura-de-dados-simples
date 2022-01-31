2d25494920412c03fcec2c27ffbfab2a13da863d
Lucas-de-Souza-Pinto <lucasdesouzapinto2000@gmail.com>
2022-01-30 22:34:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Crie um array que receba 5 itens e exiba no console.
+let _listName= ["joão","Maria","Valdisney","Jorgison","Judisney"]
+console.log(_listName)
+// Utilize um método para adicionar um nome ao inicio do array.
+_listName.unshift("Juvenal")
+// Utilize um método para remover o último nome do array.
+_listName.pop("Judisney")
+// Utilize um método para adicionar dois nomes ao fim do array.
+_listName.push("Claudineia","Yuriney")
+// Utilize um método para remover o primeiro nome do array.
+_listName.shift()
+// Utilize um método para organizar em ordem crescente o seguinte array:const numbers = [7,5,6,3,8,9,2,1,4]
+const _numbers = [7,5,6,3,8,9,2,1,4]
+_numbers.sort()
+console.log(_numbers)
